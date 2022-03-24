1af2cfa91216b8fad34153e70e408fe9083c14d2
andreaibt21 <andreaixabella@gmail.com>
2022-03-24 18:37:44 -0300

    index.tsx fixed

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
 ReactDOM.render(
-  <Router >
   <React.StrictMode>
- 
-    <App/>
-  </React.StrictMode>
-  </Router>
-  ,
-  document.getElementById('root')
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
